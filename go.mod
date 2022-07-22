c3620572dc883f07da189124e1e83927adbd4297
Mehmood Ghaffar <in1767@kit.edu>
2022-07-22 12:09:35 +0200

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/mehmood86/toolbox
+
+go 1.18
+
+require github.com/spf13/cobra v1.5.0
---
+++ main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package main
+
+import "github.com/mehmood86/toolbox/cmd"
+
+func main() {
+	cmd.Execute()
+}
